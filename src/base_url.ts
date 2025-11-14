afbe559d3d7010420caf54bc81882483e71b7b5a
Divine Abatan <abatandivine8@gmail.com>
2025-11-14 18:31:38 +0100

    pushed

--- src/base_url.ts
+++ src/base_url.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://final-year-backend-83zo.onrender.com/api/v1";
+export const API_BASE_URL =
+  "https://my-final-year-backend-project.onrender.com/api/v1";
 // export const API_BASE_URL="http://localhost:5000/api/v1"
-
